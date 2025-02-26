58183091ff8121af299e17bc228ed7c68b06d146
PedroColombo2003 <pedrocorrea087@gmail.com>
2025-02-25 21:39:14 -0300

    Add files via upload

---
+++ exercicio8.py
@@ -0,0 +1,23 @@
+"""
+Titulo: Exercicio 8
+Autor: Pedro Colombo
+Data: 25/02/2025
+Versão: 0.0.1
+Problema: resolva a equacao linear ax = b
+
+"""
+
+
+
+# Entrada de dados
+
+a = float(input("Digite o coeficiente a: "))
+b = float(input("Digite o coeficiente b: "))  
+
+# Processamento de dados
+
+x = b / a
+
+# Saida de dados
+
+print(f"O valor de X nessa equação linear é de {x}")
